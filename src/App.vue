305cab4e915d0ea67d5e7ac4257959ab5a68a334
MuammarRizal <muammarrizal31@gmail.com>
2025-01-01 23:32:24 +0700

    updated : slot component vuein Home page

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,7 @@
 <script setup>
-import { ref } from 'vue'
-import SideBar from './layouts/SideBar.vue'
-import NavBar from './layouts/NavBar.vue'
-const drawer = ref(false)
-const group = ref(null)
-
-const onCloseDrawer = () => {
-  drawer.value = !drawer.value
-}
+import Home from './views/Home.vue'
 </script>
 
 <template>
-  <v-layout>
-    <!-- navbar -->
-    <NavBar @openClose="onCloseDrawer" />
-
-    <!-- sidebar -->
-    <SideBar v-model="drawer" />
-
-    <!-- main content -->
-    <v-main style="height: 500px">
-      <v-card-text> Ini bagian Content </v-card-text>
-    </v-main>
-  </v-layout>
+  <Home />
 </template>
---
+++ src/components/AlertMessage.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-alert :type="typeValue" variant="tonal" close-label="Close Alert" closable>
+    <slot name="title" />
+    <slot name="content" />
+  </v-alert>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+defineProps({
+  typeValue: {
+    type: String,
+    required: true,
+  },
+})
+</script>
