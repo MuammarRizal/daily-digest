bb97a3857fd012b3b085b65d89a7eaec8cf5b48d
MuammarRizal <muammarrizal31@gmail.com>
2025-01-02 23:21:21 +0700

    define(store) : implement store in profile route

--- src/views/admin/Profile.view.vue
+++ src/views/admin/Profile.view.vue
@@ -3,11 +3,18 @@
     <template v-for="({ color, icon, href }, index) in attrIcon" :key="index">
       <IconComponent :color="color" :icon="icon" :href="href" />
     </template>
+
+    <ul>
+      <li v-for="data in dataUsers" :key="data.name">{{ data.name }}</li>
+    </ul>
   </div>
 </template>
 
 <script setup>
 import IconComponent from '@/components/IconLink.component.vue'
+import useUserData from '@/stores/users'
+
+const { dataUsers } = useUserData()
 const attrIcon = [
   {
     color: 'error',
