0182e3ab4a632d0ee46aa0b89228a19e99786f10
MuammarRizal <muammarrizal31@gmail.com>
2025-01-03 01:52:54 +0700

    implement(store) : in profile page

--- src/views/admin/Profile.view.vue
+++ src/views/admin/Profile.view.vue
@@ -4,6 +4,32 @@
       <IconComponent :color="color" :icon="icon" :href="href" />
     </template>
 
+    <form @submit.prevent="tambahData">
+      <label>
+        name
+        <input
+          type="text"
+          name="name"
+          id="name"
+          v-model="inputUser.name"
+          placeholder="Masukan Nama"
+        />
+      </label>
+
+      <label>
+        jurusan
+        <input
+          type="text"
+          name="jurusan"
+          id="jurusan"
+          v-model="inputUser.jurusan"
+          placeholder="Masukan Jurusan"
+        />
+      </label>
+      <button type="submit">Submit</button>
+      <br />
+    </form>
+
     <ul>
       <li v-for="data in dataUsers" :key="data.name">{{ data.name }}</li>
     </ul>
@@ -14,7 +40,7 @@
 import IconComponent from '@/components/IconLink.component.vue'
 import useUserData from '@/stores/users'
 
-const { dataUsers } = useUserData()
+const { dataUsers, tambahData, inputUser } = useUserData()
 const attrIcon = [
   {
     color: 'error',
