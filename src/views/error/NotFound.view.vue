78f4cee614ee7ca208fe4eb5cc8708f176a9c946
MuammarRizal <muammarrizal31@gmail.com>
2025-01-02 22:09:54 +0700

    add : notfound page

---
+++ src/views/error/NotFound.view.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-row justify="center">
+    <v-col cols="12" class="text-center d-flex justify-center align-center flex-column">
+      <h2 class="mt-10">Halaman Tidak Ditemukan</h2>
+      <img :src="Notfound" alt="Notfound" style="width: 80vh" class="my-10" />
+      <v-btn to="/" class="bg-purple-darken-3">Kembali</v-btn>
+    </v-col>
+  </v-row>
+</template>
+
+<script setup>
+import Notfound from '@/assets/notfound.svg'
+</script>
