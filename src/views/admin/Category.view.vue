079326bdbedf2a2d42b0c3a6de664892538e86ba
MuammarRizal <muammarrizal31@gmail.com>
2025-01-03 05:15:38 +0700

    updated : category view

--- src/views/admin/Category.view.vue
+++ src/views/admin/Category.view.vue
@@ -1,5 +1,12 @@
 <template>
-  <v-list-item :to="{ name: 'category-list', params: { name: 'politik' } }">Politik</v-list-item>
-  <v-list-item :to="{ name: 'category-list', params: { name: 'IT' } }">IT</v-list-item>
-  <v-list-item :to="{ name: 'category-list', params: { name: 'Budaya' } }">Budaya</v-list-item>
+  <v-container>
+    <!-- <v-list-item :to="{ name: 'category-list', params: { name: 'politik' } }">Politik</v-list-item>
+    <v-list-item :to="{ name: 'category-list', params: { name: 'IT' } }">IT</v-list-item>
+    <v-list-item :to="{ name: 'category-list', params: { name: 'Budaya' } }">Budaya</v-list-item> -->
+    <CategoryLayout />
+  </v-container>
 </template>
+
+<script setup>
+import CategoryLayout from '@/layouts/admin/Category.layout.vue'
+</script>
