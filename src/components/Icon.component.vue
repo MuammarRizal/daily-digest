f169c6666edb8a213f38cef89d3b7d2746740be5
MuammarRizal <muammarrizal31@gmail.com>
2025-01-02 16:46:33 +0700

    updated : Profile view add sosmed component

---
+++ src/components/Icon.component.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-btn size="large" :icon="icon" :color="color" target="_blank" :href="href" />
+</template>
+
+<script setup>
+defineProps({
+  icon: {
+    type: String,
+    required: true,
+  },
+  color: {
+    type: String,
+    required: true,
+    default: 'success',
+  },
+  href: {
+    type: String,
+    required: true,
+  },
+})
+</script>
--- src/views/Profile.view.vue
+++ src/views/Profile.view.vue
@@ -1,3 +1,26 @@
 <template>
-  <h2>Halaman Profile</h2>
+  <div class="icon-list d-flex">
+    <template v-for="({ color, icon, href }, index) in attrIcon" :key="index">
+      <IconComponent :color="color" :icon="icon" :href="href" />
+    </template>
+  </div>
 </template>
+
+<script setup>
+import IconComponent from '@/components/Icon.component.vue'
+const attrIcon = [
+  {
+    color: 'error',
+    icon: 'mdi-youtube',
+    href: 'https://youtube.com',
+  },
+  {
+    icon: 'mdi-linkedin',
+    href: 'https://linkedin.com',
+  },
+  {
+    icon: 'mdi-github',
+    href: 'https://github.com',
+  },
+]
+</script>
