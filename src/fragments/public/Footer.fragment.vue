cfe74c68d635e3fc4aa64e51e02044932a4c245b
MuammarRizal <muammarrizal31@gmail.com>
2025-01-02 20:15:38 +0700

    updated : footer in public views

--- src/fragments/public/Footer.fragment.vue
+++ src/fragments/public/Footer.fragment.vue
@@ -1,5 +1,9 @@
 <template>
-  <v-footer class="bg-purple-darken-2 d-flex text-center flex-column"
-    >@Muammar Rizal - 2024
+  <v-footer class="bg-blue-lighten-4">
+    <v-row justify="center">
+      <v-col class="text-center my-2" cols="12">
+        {{ new Date().getFullYear() }} - <strong>Muammar Rizal</strong>
+      </v-col>
+    </v-row>
   </v-footer>
 </template>
