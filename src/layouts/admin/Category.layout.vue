02d19ab4046843f64442be497fe8d6e25c97a2d4
MuammarRizal <muammarrizal31@gmail.com>
2025-01-03 18:15:40 +0700

    implement(view) : category layout to category view

--- src/layouts/admin/Category.layout.vue
+++ src/layouts/admin/Category.layout.vue
@@ -2,8 +2,22 @@
   <v-row justify="end">
     <v-btn @click="dialog = !dialog" icon="mdi-plus" class="mb-6 bg-blue-grey-darken-3" />
   </v-row>
-  <DialogComponent v-model="dialog" />
-  <TableComponent />
+  <DialogComponent v-model="dialog" width="50%">
+    <template #title>
+      <div class="title-dialog d-flex justify-space-between">
+        <h3>Tambah Data</h3>
+        <v-btn @click="dialog = !dialog" icon="mdi-window-close" color="red" />
+      </div>
+    </template>
+    <template #content>
+      <v-form @submit.prevent="onSubmit">
+        <v-text-field label="Name" v-model="category.name" />
+        <v-textarea label="Last name" v-model="category.lastname"></v-textarea>
+        <v-btn width="100%" color="green" type="submit">Tambah Data</v-btn>
+      </v-form>
+    </template>
+  </DialogComponent>
+  <TableComponent :items="desserts" />
 </template>
 
 <script setup>
@@ -11,4 +25,38 @@ import { reactive, ref } from 'vue'
 import TableComponent from '@/fragments/public/Table.component.vue'
 import DialogComponent from '@/fragments/public/Dialog.component.vue'
 const dialog = ref(false)
+
+const desserts = ref([
+  {
+    name: 'Frozen Yogurt',
+    calories: 159,
+  },
+  {
+    name: 'Ice cream sandwich',
+    calories: 237,
+  },
+  {
+    name: 'Eclair',
+    calories: 262,
+  },
+])
+const category = reactive({
+  name: '',
+  lastname: '',
+})
+
+const clearInput = () => {
+  category.name = ''
+  category.lastname = ''
+}
+
+const onSubmit = () => {
+  desserts.value.push({
+    name: category.name,
+    colories: category.lastname,
+  })
+
+  clearInput()
+  dialog.value = !dialog.value
+}
 </script>
